c8ff3eccfa46361d7e3ec94c1e14f5865f28e41e
Enock <enockzy54@gmail.com>
2026-07-21 18:11:32 +0300

    Add media files

--- config/settings.py
+++ config/settings.py
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
